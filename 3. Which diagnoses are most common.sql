7bf3fcf764a530c35cce07503ba50f96ea0b07e7
Pratyusha K <pratyusha.raok@gmail.com>
2026-06-10 00:02:54 -0400

    Delete 3. Which diagnoses are most common.sql

--- 3. Which diagnoses are most common.sql
+++
@@ -1,7 +0,0 @@
-SELECT TOP 5
-diagnosis,
-COUNT(*) AS total_patient_count
-FROM [Healthcare_Database].[dbo].[Outpatient Visits]
-WHERE diagnosis <> 'Unknown'
-GROUP BY diagnosis
-ORDER BY total_patient_count DESC
