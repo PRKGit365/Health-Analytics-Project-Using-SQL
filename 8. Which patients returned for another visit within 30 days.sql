03491af9dfd685c948a25c69957571694b3d47b2
Pratyusha K <pratyusha.raok@gmail.com>
2026-06-10 00:02:09 -0400

    Delete 8. Which patients returned for another visit within 30 days.sql

--- 8. Which patients returned for another visit within 30 days.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-DATEPART (hour, appointment_time) AS appointment_hour,
-COUNT(*) AS appointment_count
-FROM [Healthcare_Database].[dbo].Appointments
-GROUP BY
-DATEPART (hour, appointment_time)
-ORDER BY appointment_count DESC
